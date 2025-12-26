d249007c43529d65288d341f753854986c3a1a47
Sai Kumar Gangipamula <saikumargangipamula@Sais-MacBook-Air.local>
2025-12-27 02:21:13 +0400

    Add automatic image optimization to build process

--- ss-website/vite.config.ts
+++ ss-website/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteImagemin from 'vite-plugin-imagemin'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteImagemin({
+      gifsicle: { optimizationLevel: 7 },
+      optipng: { optimizationLevel: 7 },
+      mozjpeg: { quality: 90 },
+      pngquant: { quality: [0.9, 0.95] },
+      svgo: { plugins: [{ removeViewBox: false }] }
+    })
+  ],
   base: '/sstextilehub/',
 })
