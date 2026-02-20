315dcda9cdcbaba7f4fa74093072655fb206d0b4
Sai Kumar Gangipamula <saikumargangipamula@Sais-MacBook-Air.local>
2026-02-20 21:55:32 +0400

    Fix vite.config unused mode parameter

--- ss-website/vite.config.ts
+++ ss-website/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteImagemin from 'vite-plugin-imagemin'
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   plugins: [
     react(),
     viteImagemin({
